66bb0e243d5ee4b854462515ec3e0507dbdde06e
Samathingamajig <samathingamajig@gmail.com>
2021-02-24 12:56:03 -0600

    Added CLI flag structure &  flag

--- BarkScript.cpp
+++ BarkScript.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include "vendor/optionparser-1.7/optionparser.h"
 #include "token/token.h"
 #include "lexer/lexer.h"
 #include "parser/parser.h"
@@ -8,9 +9,38 @@
 #include "interpreter/interpreter.h"
 #include "context/context.h"
 
-const std::string bsversion = "0.1.5.1";
+const std::string bsversion = "0.1.6";
 
-int main() {
+enum optionIndex { CLI_UNKNOWN, CLI_HELP, CLI_NODEBUG };
+const option::Descriptor usage[] =
+{
+ {CLI_UNKNOWN, 0, "", "", option::Arg::None, "USAGE: example [options]\n\n"
+                                        "Options:" },
+ {CLI_HELP, 0, "h", "help", option::Arg::None, "  -h --help  \tPrint usage and exit." },
+ {CLI_NODEBUG, 0, "nd", "nodebug", option::Arg::None, "  -nd --nodebug  \tDoes not print Lexer or Parser results." },
+ {0,0,0,0,0,0}
+};
+
+int main(int argc, char* argv[]) {
+    argc -= (argc > 0); argv += (argc > 0); // skip program name argv[0] if present
+    option::Stats cli_stats(usage, argc, argv);
+    std::vector<option::Option> cli_options(cli_stats.options_max);
+    std::vector<option::Option> cli_buffer(cli_stats.buffer_max);
+    option::Parser cli_parse(usage, argc, argv, &cli_options[0], &cli_buffer[0]);
+
+    if (cli_parse.error())
+        return 1;
+
+    if (cli_options[CLI_HELP]) {
+        option::printUsage(std::cout, usage);
+        return 0;
+    }
+
+    bool printDebug = true;
+    if (cli_options[CLI_NODEBUG]) {
+        printDebug = false;
+    }
+
     std::cout << "BarkScript version " << bsversion << std::endl;
     spContext context = std::make_shared<Context>(Context("<main>"));
     context->symbolTable = std::make_shared<SymbolTable>(SymbolTable());
@@ -19,39 +49,48 @@ int main() {
         std::string input;
         std::cout << "bs > ";
         std::getline(std::cin, input);
-        std::cout << std::endl;
         if (std::cin.eof()) {
             return 0;
         }
         Lexer lexer = Lexer(input);
         MultiLexResult mlr = lexer.tokenize();
         if (mlr.hasError()) {
+            std::cout << std::endl;
             std::cout << mlr.error->to_string() << std::endl;
             std::cout << "--------------------------" << std::endl;
             continue;
         }
-        for (Token token : mlr.tokenized) {
-            std::cout << token.to_string() << std::endl;
+        if (printDebug) {
+            std::cout << std::endl;
+            for (Token token : mlr.tokenized) {
+                std::cout << token.to_string() << std::endl;
+            }
         }
         if (mlr.tokenized.size() != 1) {
-            std::cout << std::endl;
             Parser parser = Parser(mlr.tokenized);
             ParseResult abSyTree = parser.parse();
             if (abSyTree.hasError()) {
-                std::cout << abSyTree.error->to_string() << std::endl;
-            } else {
-                std::cout << abSyTree.node->to_string() << std::endl;
                 std::cout << std::endl;
+                std::cout << abSyTree.error->to_string() << std::endl;
+                std::cout << "--------------------------" << std::endl;
+                continue;
+            } else {
+                if (printDebug) {
+                    std::cout << std::endl;
+                    std::cout << abSyTree.node->to_string() << std::endl;
+                    std::cout << std::endl;
+                }
 
                 Interpreter interpreter;
                 RuntimeResult rt = interpreter.visit(abSyTree.node, context);
                 if (rt.hasError()) {
                     std::cout << rt.error->to_string() << std::endl;
+                    std::cout << "--------------------------" << std::endl;
                 } else {
                     std::cout << rt.object->to_string() << std::endl;
                 }
             }
         }
-        std::cout << "--------------------------" << std::endl;
+        if (printDebug) std::cout << "--------------------------" << std::endl;
     }
 }
